cc15e0408b4ebfb87f0b80179064c17008535e09
marinavaladao <148727212+marinavaladao@users.noreply.github.com>
2023-11-13 22:31:35 -0300

    Update gerenciarUsuário.hpp

--- include/gerenciarUsuário.hpp
+++ include/gerenciarUsuário.hpp
@@ -2,13 +2,13 @@
 
 #include <string>
 
-class Gerenciar_usuario {
+class Gerenciar_Usuario {
   public:
   /*
   * @brief deleta o usuario pelo id
   /*
 
-  void deletar_perfil (std::string email);
+  void Deletar_Perfil (std::string email);
 
   // verificar se existe algum perfil com esse email
   // se existir, passar por todo o arquivo onde vai ser armazenado os perfis existentes ate encontrar o email de entrada
@@ -18,7 +18,7 @@ class Gerenciar_usuario {
   * @brief exibir o cadastro pelo id 
   /*
 
-  char exibir_perfil (std::string email);
+  char Exibir_Perfil (std::string email);
 
   //verificar se existe algum perfil com esse email
   //se existir, passar por todo o arquivo onde vai ser armazenado os perfis existentes ate encontrar o email de entrada
@@ -28,4 +28,17 @@ class Gerenciar_usuario {
   * @brief alterar os dados de um perfil
   /*
 
-  bool alterar_perfil (std::string email);
+  bool Alterar_Perfil (std::string email,std::string nome,std::string livros_alugados);
+
+  //verificar se existe algum perfil com o email de entrada
+  //se existir, igualas todos os seus dados para os que entram na função acima
+
+  /*
+  * @brief imprimir todos os perfis
+  /*
+
+  char Imprimir_Perfis ();
+
+  // imprimir todos os cadastros que estão guardados no arquivo
+
+};
