fb15cecc2cfac8cbd9dc182283b1df6179745b30
gustavovdeandrade <148797062+gustavovdeandrade@users.noreply.github.com>
2023-11-13 21:12:25 -0300

    Update aluguel.hpp

--- include/aluguel.hpp
+++ include/aluguel.hpp
@@ -1,2 +1,15 @@
 #include "livro.hpp"
+#include "gerenciarAluguel.hpp"
 
+#include <string>
+
+class Aluguel {
+  public:
+  // @brief conferir se algum livro está alugado
+  void checarDisponibilidade (std:: string Titulo; std:: string Disponibilidade);
+
+  // @brief mostrar as informaçoes de aluguel do livro
+  void imprimirStatus (std:: string Titulo; std:: string Disponibilidade; std:: string Usuario;
+  std:: string Prazo)
+
+  private:
