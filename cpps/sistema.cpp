885625f717fa4442057fdb28edf0c979f42fbda1
manuanp <148726428+manuanp@users.noreply.github.com>
2023-11-17 09:20:27 -0300

    Update sistema.cpp

--- cpps/sistema.cpp
+++ cpps/sistema.cpp
@@ -1,1 +1,35 @@
+#include "sistema.hpp"
 
+Sistema::Sistema(){
+  
+}
+
+void Sistema::inicia_sistema(){
+std::cout << "Ola! Bem vindo, Administrador \n"
+<< std:: endl;
+
+  std::cout << "O que deseja fazer?"
+  << std:: endl;
+
+  carregar_opcoes(); 
+}
+
+void Sistema::carregar_opcoes(){
+  int entrada_opcoes; 
+  std::cout << "1. Cadastrar Estudante \n"
+  << std:: endl;
+  std::cout << "2. Buscar Livro \n" 
+  << std::endl;
+  std::cout << "3. Ver Acervo \n"
+  << std:: endl;
+  std::cout << "4. Configuracoes \n"
+  << std:: endl;
+
+  std::cin >> entrada_opcoes;
+  
+  switch(entrada_opcoes){
+    case 1: 
+    std::cout << "Cadastrar Estudante \n" 
+      << std::endl; 
+    
+}
