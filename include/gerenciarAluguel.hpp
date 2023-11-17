f2a4fec65dd2cf0e7e5296f18d495621538c10e3
Rafoldes <122953211+Rafoldes@users.noreply.github.com>
2023-11-17 09:33:07 -0300

    Add files via upload

---
+++ include/gerenciarAluguel.hpp
@@ -0,0 +1,21 @@
+#include "aluguel.hpp"
+#include "livro.hpp"
+#include "usuario.hpp"
+
+#include <string>
+
+class gerenciarAluguel{
+    public:
+        //alocar um livro, mudando sua chave no map do acervo para alugado
+        void alugarLivro(std::string Titulo, int prazo);
+
+        //alocar um livro, mudando sua chave no map do acervo para não alugado
+        void devolverLivro(std::string Titulo);
+
+        //aumenta o prazo estabelecido para o aluguel
+        void aumentarPrazo(int prazo, int novoPrazo);
+
+        //estipula uma multa para um usuário 
+        void definirMulta(int multa, std::string nome);
+
+};
