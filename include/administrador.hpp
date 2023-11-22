ea0311c32530a940894de18fcdb6e1b040ee1ddf
manuanp <148726428+manuanp@users.noreply.github.com>
2023-11-22 15:16:05 -0300

    Update administrador.hpp

--- include/administrador.hpp
+++ include/administrador.hpp
@@ -1,1 +1,15 @@
+#ifndef ADMINISTRADOR_HPP
+#define ADMINISTRADOR_HPP
 
+//basicamente criar um adm para fim de herança
+
+#include "perfil_usuario.hpp"
+#include <iostream>
+
+class Administrador : public Perfil_usuario {
+public:
+    Administrador(const std::string& nome, const std::string& email);
+
+    int salvar_adm();
+}; 
+#endif // ADMINISTRADOR_HPP
