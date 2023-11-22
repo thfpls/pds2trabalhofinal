e7c58703684d1e2ae27de5d5b49ff39513f03c12
gustavovdeandrade <148797062+gustavovdeandrade@users.noreply.github.com>
2023-11-22 10:06:52 -0300

    Update aluguel.hpp

--- include/aluguel.hpp
+++ include/aluguel.hpp
@@ -8,8 +8,4 @@ class Aluguel {
   // @brief conferir se algum livro está alugado, checando com a função de livro.hpp
   void checarDisponibilidade (std:: string Titulo; std:: string Disponibilidade);
 
-  // @brief alterar o status de ALUGADO para DISPONÍVEL, após um aluguel ser efetuado
-  void mudarStatus (std:: string Titulo; std:: string Disponibilidade; std:: string Usuario;
-  std:: string Prazo)
-
   private:
