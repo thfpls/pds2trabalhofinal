8b69ccde7f67a9dbc166a062c21bae50d708f2e6
gustavovdeandrade <148797062+gustavovdeandrade@users.noreply.github.com>
2023-11-22 10:07:01 -0300

    Update aluguel.hpp

--- include/aluguel.hpp
+++ include/aluguel.hpp
@@ -7,5 +7,3 @@ class Aluguel {
   public:
   // @brief conferir se algum livro está alugado, checando com a função de livro.hpp
   void checarDisponibilidade (std:: string Titulo; std:: string Disponibilidade);
-
-  private:
