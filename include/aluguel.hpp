463d23958646c174c64cff2c2ed4ca070bf860ab
gustavovdeandrade <148797062+gustavovdeandrade@users.noreply.github.com>
2023-11-21 22:28:16 -0300

    Update aluguel.hpp

--- include/aluguel.hpp
+++ include/aluguel.hpp
@@ -1,14 +1,14 @@
-#include "livro.hpp"
+#include "estudante.hpp"
 #include "gerenciarAluguel.hpp"
 
 #include <string>
 
 class Aluguel {
   public:
-  // @brief conferir se algum livro está alugado
+  // @brief conferir se algum livro está alugado, checando com a função do estudante.hpp
   void checarDisponibilidade (std:: string Titulo; std:: string Disponibilidade);
 
-  // @brief mostrar as informaçoes de aluguel do livro
+  // @brief mostrar as informaçoes de aluguel do livro, caso a função acima der como Alugado
   void imprimirStatus (std:: string Titulo; std:: string Disponibilidade; std:: string Usuario;
   std:: string Prazo)
 
