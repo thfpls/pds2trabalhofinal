384749aa13e26f0be14f907a932281da5aefa742
gustavovdeandrade <148797062+gustavovdeandrade@users.noreply.github.com>
2023-11-21 22:56:40 -0300

    Update acervo.hpp

--- include/acervo.hpp
+++ include/acervo.hpp
@@ -1,1 +1,19 @@
+#ifndef ACERVO_HPP
+#define ACERVO_HPP
 
+#include <vector>
+#include "Livro.hpp"
+
+class AcervoBiblioteca {
+public:
+    // Adiciona um livro ao acervo
+    void inserirLivro(const Livro& livro);
+
+    // Lista todos os livros no acervo
+    void verAcervo() const;
+
+private:
+    std::vector<Livro> acervo;
+};
+
+#endif // ACERVO_HPP
