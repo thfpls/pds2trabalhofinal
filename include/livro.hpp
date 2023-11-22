b8a67f94eb70eea0633fae3973f49283ca2314f1
gustavovdeandrade <148797062+gustavovdeandrade@users.noreply.github.com>
2023-11-21 22:56:11 -0300

    Update livro.hpp

--- include/livro.hpp
+++ include/livro.hpp
@@ -1,1 +1,28 @@
+#ifndef LIVRO_HPP
+#define LIVRO_HPP
 
+#include <string>
+
+class Livro {
+public:
+    Livro(const std::string& titulo, const std::string& autor, int anoPublicacao);
+
+    // Métodos de acesso
+    std::string getTitulo() const;
+    std::string getAutor() const;
+    int getAnoPublicacao() const;
+    bool estaAlugado() const;
+
+    // Métodos de modificação
+    void alterarLivro(const std::string& novoTitulo, const std::string& novoAutor, int novoAnoPublicacao);
+    void alugar();
+    void devolver();
+
+private:
+    std::string titulo;
+    std::string autor;
+    int anoPublicacao;
+    bool alugado;
+};
+
+#endif // LIVRO_HPP
