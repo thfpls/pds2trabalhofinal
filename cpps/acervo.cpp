8ab3019e6ddac802a19b54a1c2ac147c96d7e36c
gustavovdeandrade <148797062+gustavovdeandrade@users.noreply.github.com>
2023-11-21 22:57:07 -0300

    Update acervo.cpp

--- cpps/acervo.cpp
+++ cpps/acervo.cpp
@@ -1,1 +1,19 @@
+#include "Acervo.hpp"
+#include <iostream>
 
+void AcervoBiblioteca::inserirLivro(const Livro& livro) {
+    acervo.push_back(livro);
+}
+
+void AcervoBiblioteca::verAcervo() const {
+    if (acervo.empty()) {
+        std::cout << "O acervo está vazio." << std::endl;
+    } else {
+        std::cout << "Acervo da Biblioteca:" << std::endl;
+        for (const auto& livro : acervo) {
+            std::cout << "Título: " << livro.getTitulo() << ", Autor: " << livro.getAutor()
+                      << ", Ano: " << livro.getAnoPublicacao() << ", Estado: "
+                      << (livro.estaAlugado() ? "Alugado" : "Disponível") << std::endl;
+        }
+    }
+}
