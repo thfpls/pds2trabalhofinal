6b445fcda2d9b8e22749d6fe9bfbfbdb5cb11b25
gustavovdeandrade <148797062+gustavovdeandrade@users.noreply.github.com>
2023-11-21 22:57:29 -0300

    Update livro.cpp

--- cpps/livro.cpp
+++ cpps/livro.cpp
@@ -1,1 +1,34 @@
+#include "Livro.hpp"
 
+Livro::Livro(const std::string& titulo, const std::string& autor, int anoPublicacao)
+    : titulo(titulo), autor(autor), anoPublicacao(anoPublicacao), alugado(false) {}
+
+std::string Livro::getTitulo() const {
+    return titulo;
+}
+
+std::string Livro::getAutor() const {
+    return autor;
+}
+
+int Livro::getAnoPublicacao() const {
+    return anoPublicacao;
+}
+
+bool Livro::estaAlugado() const {
+    return alugado;
+}
+
+void Livro::alterarLivro(const std::string& novoTitulo, const std::string& novoAutor, int novoAnoPublicacao) {
+    titulo = novoTitulo;
+    autor = novoAutor;
+    anoPublicacao = novoAnoPublicacao;
+}
+
+void Livro::alugar() {
+    alugado = true;
+}
+
+void Livro::devolver() {
+    alugado = false;
+}
