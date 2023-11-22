cb22a9c0729d1d2c83730f0bfe959fbba1c49d6e
gustavovdeandrade <148797062+gustavovdeandrade@users.noreply.github.com>
2023-11-22 09:55:20 -0300

    Update aluguel.cpp

--- cpps/aluguel.cpp
+++ cpps/aluguel.cpp
@@ -1,1 +1,16 @@
+#include "Aluguel.hpp"
+#include "Livro.hpp"
 
+void Aluguel::checarDisponibilidade(const std::string& titulo) {
+    for (auto& livro : livros) {
+        if (livro.getTitulo() == titulo) {
+            if (livro.estaAlugado()) {
+                std::cout << "O livro está alugado. Aluguel não disponível." << std::endl;
+            } else {
+                std::cout << "O livro está disponível para aluguel." << std::endl;
+            }
+            return;
+        }
+    }
+    std::cout << "Livro não encontrado." << std::endl;
+}
