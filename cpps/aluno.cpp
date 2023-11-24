0d805517342438bbd265ffa036eb8bee1651db33
manuanp <148726428+manuanp@users.noreply.github.com>
2023-11-24 20:17:17 -0300

    Update aluno.cpp

--- cpps/aluno.cpp
+++ cpps/aluno.cpp
@@ -1,40 +1,31 @@
-//aluno.cpp
+// aluno.cpp
 #include "aluno.hpp"
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <sstream>
 
 Aluno::Aluno(const std::string& nome, const std::string& email, const std::string& matricula)
     : Perfil_usuario(nome, email), _matricula(matricula)
 {
-    // Mantém o papel padrão como ALUNO
     _papel = ALUNO;
 }
 
-const std::string& Aluno::getMatricula() const
-{
+const std::string& Aluno::getMatricula() const {
     return _matricula;
 }
 
-void Aluno::emprestarLivro(const std::string& titulo) {
-   // _livros_emprestados.pushback(titulo);
+const std::vector<std::string>& Aluno::getLivrosEmprestados() const {
+    return _livros_emprestados;
 }
 
-void Aluno::devolverLivro(const std::string& titulo) {
-
-}
-const std::vector <std::string>& Aluno::getLivrosEmprestados() const {
-    return _livros_emprestados; 
-}
-
-int Aluno::salvar_aluno() {
-    // Cria um objeto ofstream e o associa ao arquivo "usuario.csv"
+int Aluno::salvar_aluno() const {
+    // Abre o arquivo CSV para escrita (modo append)
     std::ofstream arquivo("usuario.csv", std::ios::out | std::ios::app);
 
     // Verifica se o arquivo foi aberto com sucesso
     if (!arquivo.is_open()) {
-        std::cerr << "Erro ao abrir o arquivo CSV.\n";
-        return -1; // Retorna um código de erro, se necessário
+        std::cerr << "Erro ao abrir o arquivo CSV para escrita.\n";
+        return -1;
     }
 
     // Escreve as informações do aluno no arquivo CSV
@@ -46,7 +37,45 @@ int Aluno::salvar_aluno() {
     return 0; // Retorna 0 para indicar sucesso
 }
 
-void Aluno::criarEsalvarNovoAluno() {
+bool Aluno::alunoJaExiste() const {
+    // Abre o arquivo CSV para leitura
+    std::ifstream arquivo("usuario.csv");
+
+    // Verifica se o arquivo foi aberto com sucesso
+    if (!arquivo.is_open()) {
+        std::cerr << "Erro ao abrir o arquivo CSV.\n";
+        return false;
+    }
+
+    // Formato esperado: PAPEL,NOME,EMAIL,MATRICULA
+    std::string linha;
+    while (std::getline(arquivo, linha)) {
+        // Use um stringstream para separar os campos da linha
+        std::istringstream iss(linha);
+        std::string papel, nomeArquivo, emailArquivo, matriculaArquivo;
+
+        // Leitura dos campos da linha
+        std::getline(iss, papel, ',');
+        std::getline(iss, nomeArquivo, ',');
+        std::getline(iss, emailArquivo, ',');
+        std::getline(iss, matriculaArquivo, ',');
+
+        // Comparação com os parâmetros fornecidos
+        if (papel == "ALUNO" && getNome_usuario() == nomeArquivo && getEmail_usuario() == emailArquivo && getMatricula() == matriculaArquivo) {
+            // Aluno encontrado, feche o arquivo e retorne true
+            arquivo.close();
+            return true;
+        }
+    }
+
+    // Fecha o arquivo
+    arquivo.close();
+
+    // Se não encontrar uma correspondência, retorne false
+    return false;
+}
+
+void Aluno::CadastroAluno() {
     // Solicitar informações do usuário
     std::string nome, email, matricula;
 
@@ -61,6 +90,12 @@ void Aluno::criarEsalvarNovoAluno() {
 
     // Criar um aluno dinamicamente com base nas informações do usuário
     Aluno novoAluno(nome, email, matricula);
+
+    // Verificar se o aluno já existe
+    if (novoAluno.alunoJaExiste()) {
+        std::cerr << "Aluno já existe no arquivo CSV.\n";
+        return;
+    }
 
     // Salvar as informações do aluno no arquivo CSV
     if (novoAluno.salvar_aluno() == 0) {
