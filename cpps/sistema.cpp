e8cbd8ff72800e04f443be9c0b9d18b3222f407d
manuanp <148726428+manuanp@users.noreply.github.com>
2023-11-24 19:54:47 -0300

    Update sistema.cpp

--- cpps/sistema.cpp
+++ cpps/sistema.cpp
@@ -8,6 +8,11 @@ void Sistema::inicia_sistema(){
 std::cout << "Ola! Bem vindo, Administrador \n"
 << std:: endl;
 
+  std::cout << "Se identifique:" // nao sei se isso é a melhor coisa, é pq na foto do theo tem cadastrar adm ou usuario primeiro 
+  << std:: endl;
+
+  Administrador::criarEsalvarNovoAdm();// vou ter q adaptar pra caso ja tenha cadastro 
+
   std::cout << "O que deseja fazer?"
   << std:: endl;
 
@@ -31,8 +36,19 @@ void Sistema::carregar_opcoes(){
   
   switch(entrada_opcoes){
     case 1: 
-    std::cout << "Cadastrar Estudante \n" 
+    std::cout << "Cadastrar Estudante: \n" 
       << std::endl; 
-    //
-    
-}
+    Aluno::criarEsalvarNovoAluno();
+
+    case 2: 
+    std::cout << "Buscar Livro: \n" 
+    << std::endl;
+    //chama a função e interface de busca
+
+    case 3:
+    std::cout << "Ver Acervo: \n"
+    << std:: endl;
+    //chama a função e interface de busca
+
+    case 4: //nao sei se vai existir
+   }
