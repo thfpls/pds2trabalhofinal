26d4fb547e5c7e62d0bacf3ade39a76f7bd3ce8b
manuanp <148726428+manuanp@users.noreply.github.com>
2023-11-26 16:20:24 -0300

    Update adm.cpp

--- cpps/adm.cpp
+++ cpps/adm.cpp
@@ -31,28 +31,70 @@ int Administrador::salvar_adm()
     return 0; // Retorna 0 para indicar sucesso
 }
 
-void Administrador::criarEsalvarNovoAdm()
-{
-    std::string nome, email;
+bool Administrador::admJaExiste() const {
+    // Abre o arquivo CSV para leitura
+    std::ifstream arquivo("usuario.csv");
 
-    // Solicitar informações do administrador
-    std::cout << "Digite o nome do administrador: ";
+    // Verifica se o arquivo foi aberto com sucesso
+    if (!arquivo.is_open()) {
+        std::cerr << "Erro ao abrir o arquivo CSV.\n";
+        return false;
+    }
+
+    // Formato esperado: PAPEL,NOME,EMAIL;
+    std::string linha;
+    while (std::getline(arquivo, linha)) {
+        // Use um stringstream para separar os campos da linha
+        std::istringstream iss(linha);
+        std::string papel, nomeArquivo, emailArquivo;
+
+        // Leitura dos campos da linha
+        std::getline(iss, papel, ',');
+        std::getline(iss, nomeArquivo, ',');
+        std::getline(iss, emailArquivo, ',');
+
+        // Comparação com os parâmetros fornecidos
+        if (papel == "ADMIN" && getNome_usuario() == nomeArquivo && getEmail_usuario() == emailArquivo) {
+            // Adm encontrado, feche o arquivo e retorne true
+            arquivo.close();
+            return true;
+        }
+    }
+
+    // Fecha o arquivo
+    arquivo.close();
+
+    // Se não encontrar uma correspondência, retorne false
+    return false;
+}
+
+void Administrador::CadastrarAdm() {
+    // Solicitar informações do usuário
+    std::string nome, email, matricula;
+
+    std::cout << "Digite o nome do aluno: ";
     std::getline(std::cin, nome);
 
-    std::cout << "Digite o email do administrador: ";
-    std::getline(std::cin,email);
+    std::cout << "Digite o email do aluno: ";
+    std::getline(std::cin, email);
 
-    //criar um amd dinamicamente
+    std::cout << "Digite a matrícula do aluno: ";
+    std::getline(std::cin, matricula);
 
-    Administrador novoAdministrador(nome, email); 
+    // Criar um aluno dinamicamente com base nas informações do usuário
+    Aluno novoAluno(nome, email, matricula);
 
-    // Salvar as informações do administrador no arquivo CSV
-    if (novoAdministrador.salvar_adm() == 0)
-    {
-        std::cout << "Administrador salvo no arquivo CSV com sucesso.\n";
+    // Verificar se o aluno já existe
+    if (novoAluno.alunoJaExiste()) {
+        std::cerr << "Aluno já existe no arquivo CSV.\n";
+        return;
     }
-    else
-    {
-        std::cerr << "Erro ao salvar o administrador no arquivo CSV.\n";
+
+    // Salvar as informações do aluno no arquivo CSV
+    if (novoAluno.salvar_aluno() == 0) {
+        std::cout << "Aluno salvo no arquivo CSV com sucesso.\n";
+    }
+    else {
+        std::cerr << "Erro ao salvar o aluno no arquivo CSV.\n";
     }
 }
