fceacd9bd28d47164b7bbb5bfb356f5af4dba718
Rafoldes <122953211+Rafoldes@users.noreply.github.com>
2023-11-28 15:13:31 -0300

    Update gerenciarAluguel.cpp

--- cpps/gerenciarAluguel.cpp
+++ cpps/gerenciarAluguel.cpp
@@ -1,23 +1,19 @@
 #include "gerenciarAluguel.hpp"
 
-void GerenciarAluguel::alugarLivro(const std::string& titulo, const std::string& disponibilidade) {
-    // Cria uma chave para o livro usando título e disponibilidade
-    std::pair<std::string, std::string> chaveLivro = std::make_pair(titulo, disponibilidade);
+std::map<std::pair<std::string, std::string>, std::pair<int, double>> GerenciarAluguel::livrosAlugados;
 
-    // Adiciona o livro ao mapa de livros alugados com prazo de devolução inicial de 7 dias e multa inicial de 0
+void GerenciarAluguel::alugarLivro(const std::string& titulo, const std::string& disponibilidade) {
+    std::pair<std::string, std::string> chaveLivro = std::make_pair(titulo, disponibilidade);
     livrosAlugados[chaveLivro] = std::make_pair(7, 0.0);
 
     std::cout << "Livro '" << titulo << "' alugado com sucesso." << std::endl;
 }
 
 void GerenciarAluguel::devolverLivro(const std::string& titulo, const std::string& disponibilidade) {
-    // Criação de  uma chave para o livro usando título e disponibilidade
     std::pair<std::string, std::string> chaveLivro = std::make_pair(titulo, disponibilidade);
 
-    // Verificação de se o livro está presente no mapa
     auto it = livrosAlugados.find(chaveLivro);
     if (it != livrosAlugados.end()) {
-        // Remove o livro do mapa
         livrosAlugados.erase(it);
         std::cout << "Livro '" << titulo << "' devolvido com sucesso." << std::endl;
     } else {
@@ -26,13 +22,10 @@ void GerenciarAluguel::devolverLivro(const std::string& titulo, const std::strin
 }
 
 void GerenciarAluguel::aumentarPrazo(const std::string& titulo, const std::string& disponibilidade, int dias) {
-    // Criação de uma chave para o livro usando título e disponibilidade
     std::pair<std::string, std::string> chaveLivro = std::make_pair(titulo, disponibilidade);
 
-    // Verifica se o livro está presente no mapa
     auto it = livrosAlugados.find(chaveLivro);
     if (it != livrosAlugados.end()) {
-        // Aumenta o prazo de devolução
         it->second.first += dias;
         std::cout << "Prazo de devolução do livro '" << titulo << "' aumentado para " << it->second.first << " dias." << std::endl;
     } else {
@@ -41,13 +34,10 @@ void GerenciarAluguel::aumentarPrazo(const std::string& titulo, const std::strin
 }
 
 void GerenciarAluguel::definirMulta(const std::string& titulo, const std::string& disponibilidade, double multa) {
-    // Criação de uma chave para o livro usando título e disponibilidade
     std::pair<std::string, std::string> chaveLivro = std::make_pair(titulo, disponibilidade);
 
-    // Verificação de se o livro está presente no mapa
     auto it = livrosAlugados.find(chaveLivro);
     if (it != livrosAlugados.end()) {
-        // Define a multa para o livro
         it->second.second = multa;
         std::cout << "Multa para o livro '" << titulo << "' definida como " << multa << "." << std::endl;
     } else {
