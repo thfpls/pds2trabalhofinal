5043cabb837103a108c855639a2568fbea99a6fd
gustavovdeandrade <148797062+gustavovdeandrade@users.noreply.github.com>
2023-11-28 11:00:06 -0300

    Update livro.cpp

--- cpps/livro.cpp
+++ cpps/livro.cpp
@@ -19,13 +19,46 @@ bool Livro::estaAlugado() const {
     return disponivel;
 }
 
+#include "Acervo.hpp"
+#include <iostream>
 
+void AcervoBiblioteca::alterarLivro() {
+    using namespace std;
 
-void Livro::alterarLivro(const std::string& novoTitulo, const std::string& novoAutor, int novoAnoPublicacao) {
-    titulo = novoTitulo;
-    autor = novoAutor;
-    anoPublicacao = novoAnoPublicacao;
+    // Declara variável para armazenar o título do livro a ser alterado
+    string titulo;
+    cout << "Digite o título do livro a ser alterado: ";
+    getline(cin, titulo);
+
+    // Realiza a busca no acervo pelo título
+    auto it = std::find_if(acervo.begin(), acervo.end(), [titulo](const Livro& livro) {
+        return livro.getTitulo() == titulo;
+    });
+
+    if (it != acervo.end()) {
+        // Livro encontrado, solicita as novas informações
+        string novoTitulo, novoAutor;
+        int novoAnoPublicacao;
+
+        cout << "Digite o novo título: ";
+        getline(cin, novoTitulo);
+        cout << "Digite o novo autor: ";
+        getline(cin, novoAutor);
+        cout << "Digite o novo ano de publicacao: ";
+        cin >> novoAnoPublicacao;
+
+        // Limpa o buffer do teclado após a leitura do ano
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        // Altera as informações do livro
+        it->alterarLivro(novoTitulo, novoAutor, novoAnoPublicacao);
+
+        cout << "Livro alterado com sucesso." << endl;
+    } else {
+        cout << "Livro não encontrado no acervo." << endl;
+    }
 }
+
 
 void Livro::alugar() {
 /*    if (!alugado) {
