cf193fe023b2df61c2a88e59c480e38c3018c360
manuanp <148726428+manuanp@users.noreply.github.com>
2023-11-29 19:51:50 -0300

    Update and rename perfil_usuario.cpp to perfilUsuario.cpp

---
+++ cpps/perfilUsuario.cpp
@@ -0,0 +1,28 @@
+#include "perfilUsuario.hpp"
+#include "aluno.hpp"
+//implementar o construtor
+
+PerfilUsuario::PerfilUsuario(const std::string& nome, const std::string& email) : _nome_perfil_usuario(nome), _email_perfil_usuario(email), _papel(ALUNO)
+{
+}
+
+//métodos
+const std::string& PerfilUsuario::getNomeUsuario() const {
+    return _nome_perfil_usuario;
+}
+
+const std::string& PerfilUsuario::getEmailUsuario() const {
+    return _email_perfil_usuario;
+}
+
+Papel_do_usuario Perfil_usuario::getPapelUsuario() const {
+    return _papel;
+}
+
+std::string PerfilUsuario::papelToString() const {
+    return (_papel == ADMIN) ? "ADMIN" : "ALUNO";
+}
+// destrutor virtual
+PerfilUsuario::~PerfilUsuario() {
+
+}
