43ab02d4909f4e4d0f3a87d165c743d584ad1c39
Rafoldes <122953211+Rafoldes@users.noreply.github.com>
2023-11-28 22:16:47 -0300

    Update gerenciarAluguel.cpp

--- cpps/gerenciarAluguel.cpp
+++ cpps/gerenciarAluguel.cpp
@@ -3,6 +3,25 @@
 
 std::vector<Livro> GerenciarAluguel::livros;
 
+// gerenciarAluguel.cpp
+#include "gerenciarAluguel.hpp"
+#include <iostream>
+
+void GerenciarAluguel::alugarLivro(const std::string& titulo, const std::string& usuario) {
+    livrosAlugados[usuario].insert(titulo);
+    std::cout << "Livro '" << titulo << "' alugado por '" << usuario << "'." << std::endl;
+}
+
+void GerenciarAluguel::devolverLivro(const std::string& titulo, const std::string& usuario) {
+    auto it = livrosAlugados[usuario].find(titulo);
+    if (it != livrosAlugados[usuario].end()) {
+        livrosAlugados[usuario].erase(it);
+        std::cout << "Livro '" << titulo << "' devolvido por '" << usuario << "'." << std::endl;
+    } else {
+        std::cout << "Livro '" << titulo << "' não está na lista de livros alugados por '" << usuario << "'." << std::endl;
+    }
+}
+
 void GerenciarAluguel::aumentarPrazo(const std::string& titulo, int dias) {
     auto it = std::find_if(livros.begin(), livros.end(), [titulo](const Livro& livro) {
         return livro.getTitulo() == titulo && livro.estaAlugado();
