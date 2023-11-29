6d631d651ef70e716e5c13a5b19be884a387e696
Rafoldes <122953211+Rafoldes@users.noreply.github.com>
2023-11-29 18:25:12 -0300

    Update gerenciarAluguel.cpp

--- cpps/gerenciarAluguel.cpp
+++ cpps/gerenciarAluguel.cpp
@@ -1,45 +1,79 @@
+// gerenciarAluguel.cpp
 #include "gerenciarAluguel.hpp"
 #include <iostream>
+#include <algorithm>
 
 std::vector<Livro> GerenciarAluguel::livros;
 
-void GerenciarAluguel::alugarLivro(const std::string& titulo, const std::string& usuario) {
-    livrosAlugados[usuario].insert(titulo);
-    std::cout << "Livro '" << titulo << "' alugado por '" << usuario << "'." << std::endl;
+void GerenciarAluguel::alugarLivro(const std::string& titulo) {
+    auto it = std::find_if(livros.begin(), livros.end(), [titulo](const Livro& livro) {
+        return livro.getTitulo() == titulo;
+    });
+
+    if (it != livros.end()) {
+        if (!it->estaAlugado()) {
+            it->alugar();
+            std::cout << "Livro alugado com sucesso." << std::endl;
+        } else {
+            std::cout << "Livro indisponível para aluguel, já está alugado." << std::endl;
+        }
+    } else {
+        std::cout << "Livro não encontrado." << std::endl;
+    }
 }
 
-void GerenciarAluguel::devolverLivro(const std::string& titulo, const std::string& usuario) {
-    auto it = livrosAlugados[usuario].find(titulo);
-    if (it != livrosAlugados[usuario].end()) {
-        livrosAlugados[usuario].erase(it);
-        std::cout << "Livro '" << titulo << "' devolvido por '" << usuario << "'." << std::endl;
+void GerenciarAluguel::devolverLivro(const std::string& titulo) {
+    auto it = std::find_if(livros.begin(), livros.end(), [titulo](const Livro& livro) {
+        return livro.getTitulo() == titulo;
+    });
+
+    if (it != livros.end()) {
+        if (it->estaAlugado()) {
+            it->devolver();
+            std::cout << "Livro devolvido com sucesso." << std::endl;
+        } else {
+            std::cout << "Livro não está alugado, não pode ser devolvido." << std::endl;
+        }
     } else {
-        std::cout << "Livro '" << titulo << "' não está na lista de livros alugados por '" << usuario << "'." << std::endl;
+        std::cout << "Livro não encontrado." << std::endl;
     }
 }
 
 void GerenciarAluguel::aumentarPrazo(const std::string& titulo, int dias) {
     auto it = std::find_if(livros.begin(), livros.end(), [titulo](const Livro& livro) {
-        return livro.getTitulo() == titulo && livro.estaAlugado();
+        return livro.getTitulo() == titulo;
     });
 
     if (it != livros.end()) {
         it->aumentarPrazo(dias);
-        std::cout << "Prazo de devolução do livro '" << titulo << "' aumentado para " << it->getPrazoDevolucao() << " dias." << std::endl;
+        std::cout << "Prazo aumentado com sucesso." << std::endl;
     } else {
-        std::cout << "Livro '" << titulo << "' não encontrado ou não está alugado." << std::endl;
+        std::cout << "Livro não encontrado." << std::endl;
     }
 }
 
 void GerenciarAluguel::definirMulta(const std::string& titulo, double multa) {
     auto it = std::find_if(livros.begin(), livros.end(), [titulo](const Livro& livro) {
-        return livro.getTitulo() == titulo && livro.estaAlugado();
+        return livro.getTitulo() == titulo;
     });
 
     if (it != livros.end()) {
         it->definirMulta(multa);
-        std::cout << "Multa para o livro '" << titulo << "' definida como " << multa << "." << std::endl;
+        std::cout << "Multa definida com sucesso." << std::endl;
     } else {
-        std::cout << "Livro '" << titulo << "' não encontrado ou não está alugado." << std::endl;
+        std::cout << "Livro não encontrado." << std::endl;
+    }
+}
+
+bool GerenciarAluguel::checarDisponibilidade(const std::string& titulo) {
+    auto it = std::find_if(livros.begin(), livros.end(), [titulo](const Livro& livro) {
+        return livro.getTitulo() == titulo;
+    });
+
+    if (it != livros.end()) {
+        return !it->estaAlugado();
+    } else {
+        std::cout << "Livro não encontrado." << std::endl;
+        return false;
     }
 }
