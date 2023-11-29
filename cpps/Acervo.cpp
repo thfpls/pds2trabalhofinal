1693c2d3998019d60adc11fade7de6722dc9d9f7
thfpls <86666792+thfpls@users.noreply.github.com>
2023-11-29 15:32:30 -0300

    Update Acervo.cpp

--- cpps/Acervo.cpp
+++ cpps/Acervo.cpp
@@ -1,22 +1,23 @@
 #include "Acervo.hpp"
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 void Acervo::inserirLivro() {
     using namespace std;
 
     // Declarado o objeto pro arquivo
-    ofstream arquivo;
+    ifstream arquivo_in;
 
-    // abre "acervo.csv" modo de output e append(botar novo no arquivo)
-    arquivo.open("acervo.csv", ios::out | ios::app);
+    // abre "acervo.csv" modo de input
+    arquivo_in.open("acervo.csv", ios::in);
 
     // checa se abriu direitin
-    if (arquivo.is_open())
+    if (arquivo_in.is_open())
     {
         // declara variaveis pra estocar o input do user
         string titulo, autor, anoPublicacao;
-
+        
         // Prompt o usuario a botar dados
         cout << "Digite o titulo do livro: ";
         getline(cin, titulo);
@@ -24,6 +25,69 @@ void Acervo::inserirLivro() {
         getline(cin, autor);
         cout << "Digite o ano de publicacao do livro: ";
         getline(cin, anoPublicacao);
+        
+           // declara um vetor pra armazenar as linhas do arquivo
+        vector<string> linhas;
+
+        // declara uma string pra armazenar uma linha do arquivo
+        string linha;
+
+        // le o arquivo e armazena as linhas no vetor
+        while (getline(arquivo_in, linha, '\n'))
+        {
+            linhas.push_back(linha);
+        }
+
+        // fecha o arquivo de input
+        arquivo_in.close();
+
+        // declara uma variavel booleana pra indicar se o livro ja existe
+        bool existe = false;
+
+        // percorre o vetor de linhas
+        for (int i = 0; i < linhas.size(); i++)
+        {
+            // declara um stringstream pra separar os campos do livro
+            stringstream ss(linhas[i]);
+
+            // declara um vetor pra armazenar os campos do livro
+            vector<string> campos;
+
+            // declara uma string pra armazenar um campo do livro
+            string campo;
+
+            // le o stringstream e armazena os campos no vetor
+            while (getline(ss, campo, ','))
+            {
+                campos.push_back(campo);
+            }
+
+            // compara os campos do livro com os parametros do usuario
+            if (campos[0] == titulo || campos[1] == autor || campos[2] == anoPublicacao)
+            {
+                // se algum campo for igual, o livro ja existe
+                existe = true;
+
+                // mostra uma mensagem de erro
+                cout << "Erro: O livro ja existe no acervo.\n";
+
+                // sai do loop
+                break;
+            }
+        }
+
+        if (!existe) {
+        // Declarado o objeto pro arquivo
+        ofstream arquivo;
+
+        // abre "acervo.csv" modo de output e append(botar novo no arquivo)
+    arquivo.open("acervo.csv", ios::out | ios::app);
+        
+    // checa se abriu direitin
+    if (arquivo.is_open())
+    {
+        // declara variaveis pra estocar o input do user
+        string titulo, autor, anoPublicacao;
 
         // bota o livro no arquivo csv
         arquivo << titulo << ", " << autor << ", " << anoPublicacao << ", " << "1" << "\n";
@@ -42,9 +106,9 @@ void Acervo::inserirLivro() {
         // mostra erro
         cout << "Erro: Não foi possível abrir o arquivo.\n";
     }
-
-    return 0;
-}
+        }
+    return;
+    }
 
 void Acervo::verAcervo() const {
     if (acervo.empty()) {
