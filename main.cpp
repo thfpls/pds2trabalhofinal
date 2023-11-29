bb5b0f4a62ecaa3640d27ea6e0f8b4a661e4b37e
gustavovdeandrade <148797062+gustavovdeandrade@users.noreply.github.com>
2023-11-29 04:37:07 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "acervo.cpp"
+#include "adm.cpp"
+#include "aluguel.cpp"
+#include "aluno.cpp"
+#include "gerenciarAluguel.cpp"
+#include "gerenciarUsuario.cpp"
+#include "livro.cpp"
+#include "perfil_uduario.cpp"
+#include "sistema.cpp"
+
+int main(){
+    inicia_sistema();
+    return 0;
+}
