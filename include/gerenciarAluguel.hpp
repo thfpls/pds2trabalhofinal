024e33a8aa466e676066dd22f30ca3e082521831
Rafoldes <122953211+Rafoldes@users.noreply.github.com>
2023-11-28 22:01:23 -0300

    Update gerenciarAluguel.hpp

--- include/gerenciarAluguel.hpp
+++ include/gerenciarAluguel.hpp
@@ -1,26 +1,21 @@
+
 #ifndef GERENCIAR_ALUGUEL_HPP
 #define GERENCIAR_ALUGUEL_HPP
 
-#include <map>
-#include <string>
 #include <iostream>
+#include <vector>
+#include <string>
+#include "livro.hpp"
 
 class GerenciarAluguel {
 public:
-    // Adiciona um livro ao mapa de livros alugados
-    void alugarLivro(const std::string& titulo, const std::string& disponibilidade);
-    // Remove um livro do mapa de livros alugados
-    void devolverLivro(const std::string& titulo, const std::string& disponibilidade);
+    static std::vector<Livro> livros;  // Alteração: Usaremos um vetor de Livro em vez de um mapa
 
-    // Aumenta o prazo de devolução para um livro específico
-    void aumentarPrazo(const std::string& titulo, const std::string& disponibilidade, int dias);
-
-    // Define uma multa para um livro específico
-    void definirMulta(const std::string& titulo, const std::string& disponibilidade, double multa);
-
-private:
-    // Mapa para armazenar livros alugados, a chave é uma combinação de título e disponibilidade
-    std::map<std::pair<std::string, std::string>, std::pair<int, double>> livrosAlugados;
+    static void alugarLivro(const std::string& titulo);
+    static void devolverLivro(const std::string& titulo);
+    static void aumentarPrazo(const std::string& titulo, int dias);
+    static void definirMulta(const std::string& titulo, double multa);
+    static bool checarDisponibilidade(const std::string& titulo);
 };
 
 #endif // GERENCIAR_ALUGUEL_HPP
