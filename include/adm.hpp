037b9a1a929d77102548ba3cbab943660bdeeb96
manuanp <148726428+manuanp@users.noreply.github.com>
2023-11-30 13:46:45 -0300

    Update adm.hpp

--- include/adm.hpp
+++ include/adm.hpp
@@ -6,18 +6,32 @@
 #include <iostream>
 #include <string>
 
+/**
+ * @brief Classe que representa um Administrador, derivada da classe PerfilUsuario.
+ */
 class Administrador : public PerfilUsuario
 {
 private:
-    std::string _nome_adm;
-    std::string _email_adm;
+    std::string _nome_adm; /**< Nome do Administrador. */
+    std::string _email_adm; /**< E-mail do Administrador. */
 
 public:
+    
+     /// @brief Construtor da classe Administrador.
+     /// @param nome Nome do Administrador.
+     /// @param email E-mail do Administrador.
     Administrador(const std::string& nome, const std::string& email);
 
+     /// @brief Salva as informações do Administrador em um arquivo CSV.
+     /// @return 0 em caso de sucesso, -1 em caso de falha.
     int salvarAdm();
+
+     /// @brief Verifica se um Administrador já existe no arquivo CSV.
+     /// @return True se o Administrador já existe, False caso contrário
     bool admJaExiste() const;
 
+    
+     /// @brief Realiza o cadastro de um novo Administrador.
     static void cadastroAdm();
 };
 
