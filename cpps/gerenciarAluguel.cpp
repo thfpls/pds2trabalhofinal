8b16fb5306c6fad1d724f155612caf211d078ae4
Rafoldes <122953211+Rafoldes@users.noreply.github.com>
2023-12-01 19:21:13 -0300

    Update gerenciarAluguel.cpp

--- cpps/gerenciarAluguel.cpp
+++ cpps/gerenciarAluguel.cpp
@@ -21,9 +21,19 @@ void GerenciarAluguel::alugarLivro(const std::string& titulo) {
     if (it != livros.end()) {
         if (!it->estaAlugado()) {
             it->alugar();
+
+            // Adiciona a informação do aluguel no arquivo CSV
+            std::ofstream arquivo("alugueis.csv", std::ios::out | std::ios::app);
+            if (arquivo.is_open()) {
+                arquivo << titulo << "," << "1" << "\n";
+                arquivo.close();
+            } else {
+                std::cerr << "Erro ao abrir o arquivo de alugueis." << std::endl;
+            }
+
             std::cout << "Livro alugado com sucesso." << std::endl;
         } else {
-            std::cout << "Livro indisponível para aluguel, já está alugado." << std::endl;
+            std::cout << "Livro indisponível para aluguel." << std::endl;
         }
     } else {
         std::cout << "Livro não encontrado." << std::endl;
@@ -38,15 +48,41 @@ void GerenciarAluguel::devolverLivro(const std::string& titulo) {
     if (it != livros.end()) {
         if (it->estaAlugado()) {
             it->devolver();
-            std::cout << "Livro devolvido com sucesso." << std::endl;
+
+            // Remove a informação do aluguel do arquivo CSV temporário
+            std::ifstream arquivoOriginal("alugueis.csv");
+            std::ofstream arquivoTemporario("alugueis_temp.csv");
+
+            if (arquivoOriginal.is_open() && arquivoTemporario.is_open()) {
+                std::string linha;
+                while (std::getline(arquivoOriginal, linha)) {
+                    std::istringstream iss(linha);
+                    std::string livroAlugado;
+                    std::getline(iss, livroAlugado, ',');
+
+                    if (livroAlugado != titulo) {
+                        arquivoTemporario << linha << "\n";
+                    }
+                }
+
+                arquivoOriginal.close();
+                arquivoTemporario.close();
+
+                // Remove o arquivo original e renomeia o temporário
+                std::remove("alugueis.csv");
+                std::rename("alugueis_temp.csv", "alugueis.csv");
+
+                std::cout << "Livro devolvido com sucesso." << std::endl;
+            } else {
+                std::cerr << "Erro ao abrir os arquivos de alugueis." << std::endl;
+            }
         } else {
-            std::cout << "Livro não está alugado, não pode ser devolvido." << std::endl;
+            std::cout << "Livro já está disponível, não pode ser devolvido." << std::endl;
         }
     } else {
         std::cout << "Livro não encontrado." << std::endl;
     }
 }
-
 void GerenciarAluguel::aumentarPrazo(const std::string& titulo, int dias) {
     auto it = std::find_if(livros.begin(), livros.end(), [titulo](const Livro& livro) {
         return livro.getTitulo() == titulo;
