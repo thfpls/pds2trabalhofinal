dc5f5446072efb96a4d842040a46f7bbbd18b388
Rafoldes <122953211+Rafoldes@users.noreply.github.com>
2023-12-01 20:52:17 -0300

    Update livro.hpp

--- include/livro.hpp
+++ include/livro.hpp
@@ -15,6 +15,8 @@ public:
 
     // @brief funções de alteração dos dados existentes de um livro
     void alterarLivro(const std::string& novoTitulo, const std::string& novoAutor, int novoAnoPublicacao);
+    void alugar();
+    void devolver();
 
 private:
     std::string titulo;
