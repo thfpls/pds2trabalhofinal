a111d49340bb8d0db7883b33f1e2c93143f7b985
gustavovdeandrade <148797062+gustavovdeandrade@users.noreply.github.com>
2023-12-02 11:37:56 -0300

    Update aluno.hpp

--- include/aluno.hpp
+++ include/aluno.hpp
@@ -1,43 +1,58 @@
-// aluno.hpp
 #ifndef ALUNO_HPP
 #define ALUNO_HPP
 
-#include "perfilUsuario.hpp"
-#include <vector>
-#include <string>
+#include "PerfilUsuario.hpp"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cctype>
 
-///@brief Classe que representa um Aluno, derivada da classe PerfilUsuario
+/**
+ * @brief Classe que representa um Aluno, derivada da classe PerfilUsuario.
+ *
+ * A classe Aluno possui funcionalidades específicas para alunos,
+ * como salvar informações no arquivo CSV, verificar se um aluno já existe
+ * e realizar o cadastro de um novo aluno.
+ */
 class Aluno : public PerfilUsuario {
-private:
-    std::string _matricula; 
-    std::vector<std::string> _livros_emprestados; 
-
 public:
-    
-     /// @brief Construtor da classe Aluno
-     /// @param nome Nome do Aluno
-     /// @param email E-mail do Aluno
-     /// @param matricula Matrícula do Aluno
+    /**
+     * @brief Construtor da classe Aluno.
+     *
+     * @param nome O nome do aluno.
+     * @param email O email do aluno.
+     * @param matricula A matrícula do aluno.
+     */
     Aluno(const std::string& nome, const std::string& email, const std::string& matricula);
 
-     /// @brief Obtém a matrícula do Aluno
-     // @return Matrícula do Aluno
+    /**
+     * @brief Obtém a matrícula do aluno.
+     *
+     * @return A matrícula do aluno.
+     */
     const std::string& getMatricula() const;
-    
-     /// @brief Obtém a lista de livros emprestados pelo Aluno
-     /// @return Lista de livros emprestados pelo Aluno
-    const std::vector<std::string>& getLivrosEmprestados() const;
 
-     /// @brief Salva as informações do Aluno em um arquivo CSV
-     /// @return 0 em caso de sucesso, -1 em caso de falha
+    /**
+     * @brief Salva as informações do aluno no arquivo CSV.
+     *
+     * @return 0 em caso de sucesso, 1 em caso de erro.
+     */
     int salvarAluno() const;
 
-     /// @brief Verifica se um Aluno já existe no arquivo CSV
-     /// @return True se o Aluno já existe, False caso contrário
+    /**
+     * @brief Verifica se um aluno com as mesmas informações já existe.
+     *
+     * @return true se o aluno já existe, false caso contrário.
+     */
     bool alunoJaExiste() const;
 
-     /// @brief Realiza o cadastro de um novo Aluno
-    static void cadastroAluno();
+    /**
+     * @brief Realiza o cadastro de um novo aluno.
+     *
+     * Solicita informações do usuário, verifica se o aluno já existe
+     * e, se não existir, salva as informações no arquivo CSV.
+     */
+    void cadastroAluno();
 };
 
 #endif // ALUNO_HPP
