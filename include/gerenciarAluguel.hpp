f42b77f774977e1350f3c9113b73b6039d3e959e
gustavovdeandrade <148797062+gustavovdeandrade@users.noreply.github.com>
2023-12-02 11:20:14 -0300

    Update gerenciarAluguel.hpp

--- include/gerenciarAluguel.hpp
+++ include/gerenciarAluguel.hpp
@@ -21,14 +21,14 @@ public:
      *
      * @param titulo O título do livro a ser alugado.
      */
-    static void alugarLivro(const std::string& titulo);
+    void alugarLivro(const std::string& titulo);
 
     /**
      * @brief Devolve um livro previamente alugado.
      *
      * @param titulo O título do livro a ser devolvido.
      */
-    static void devolverLivro(const std::string& titulo);
+    void devolverLivro(const std::string& titulo);
 
     /**
      * @brief Aumenta o prazo de devolução de um livro.
@@ -36,7 +36,7 @@ public:
      * @param titulo O título do livro para o qual o prazo será aumentado.
      * @param dias   O número de dias a serem adicionados ao prazo de devolução.
      */
-    static void aumentarPrazo(const std::string& titulo, int dias);
+    void aumentarPrazo(const std::string& titulo, int dias);
 
     /**
      * @brief Define a multa para um livro específico.
@@ -44,7 +44,7 @@ public:
      * @param titulo O título do livro para o qual a multa será definida.
      * @param multa  O valor da multa a ser aplicada.
      */
-    static void definirMulta(const std::string& titulo, double multa);
+    void definirMulta(const std::string& titulo, double multa);
 
     /**
      * @brief Verifica a disponibilidade de um livro.
