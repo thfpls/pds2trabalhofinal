6e79f1b0382b261d431c6fc2b4d7d1ac0e7a778c
gustavovdeandrade <148797062+gustavovdeandrade@users.noreply.github.com>
2023-12-02 11:24:45 -0300

    Update acervo.hpp

--- include/acervo.hpp
+++ include/acervo.hpp
@@ -1,22 +1,56 @@
-#ifndef ACERVO_HPP
-#define ACERVO_HPP
+#ifndef ACERVO_BIBLIOTECA_HPP
+#define ACERVO_BIBLIOTECA_HPP
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
 #include <vector>
-#include "livro.hpp"
+#include "Livro.hpp"
 
+/**
+ * @brief Classe para gerenciar o acervo de uma biblioteca.
+ *
+ * A classe AcervoBiblioteca permite a inserção, visualização, busca e armazenamento
+ * de livros em um acervo, utilizando um arquivo CSV como persistência.
+ */
 class AcervoBiblioteca {
 public:
-    // Adiciona um livro ao acervo (para o csv e para o vetor no runtime)
+    /**
+     * @brief Insere um novo livro no acervo.
+     *
+     * Permite ao usuário inserir um novo livro no acervo, verificando se o livro
+     * já existe antes de adicioná-lo.
+     */
     void inserirLivro();
 
-    // Lista todos os livros no acervo
+    /**
+     * @brief Exibe o acervo da biblioteca.
+     *
+     * Mostra no console os detalhes de todos os livros presentes no acervo.
+     */
     void verAcervo() const;
 
-    // Carrega o acervo pro vector na classe sistema
-    void botarNoVetor();
+    /**
+     * @brief Busca por um livro no acervo com base no título.
+     *
+     * Solicita ao usuário o título do livro desejado e exibe as informações se encontrado.
+     */
+    void buscarLivro() const;
 
 private:
+    /**
+     * @brief Armazena os livros presentes no acervo.
+     */
     std::vector<Livro> acervo;
+
+    /**
+     * @brief Insere um livro no vetor acervo.
+     *
+     * @param titulo          O título do livro.
+     * @param autor           O autor do livro.
+     * @param anoPublicacao   O ano de publicação do livro.
+     */
+    void botarNoVetor(std::string titulo, std::string autor, int anoPublicacao);
 };
 
-#endif // ACERVO_HPP
+#endif // ACERVO_BIBLIOTECA_HPP
