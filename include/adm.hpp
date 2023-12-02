433602e17f917714df3ad0be0ebf64f02fbae59d
gustavovdeandrade <148797062+gustavovdeandrade@users.noreply.github.com>
2023-12-02 11:33:03 -0300

    Update adm.hpp

--- include/adm.hpp
+++ include/adm.hpp
@@ -1,37 +1,49 @@
-// adm.hpp
 #ifndef ADMINISTRADOR_HPP
 #define ADMINISTRADOR_HPP
 
-#include "perfilUsuario.hpp"
+#include "PerfilUsuario.hpp"
 #include <iostream>
-#include <string>
+#include <fstream>
+#include <sstream>
 
-
- ///@brief Classe que representa um Administrador, derivada da classe PerfilUsuario
-class Administrador : public PerfilUsuario
-{
-private:
-    std::string _nome_adm; 
-    std::string _email_adm; 
-
+/**
+ * @brief Classe que representa um Administrador, derivada da classe PerfilUsuario.
+ *
+ * A classe Administrador possui funcionalidades específicas para administradores,
+ * como salvar informações no arquivo CSV, verificar se um administrador já existe
+ * e realizar o cadastro de um novo administrador.
+ */
+class Administrador : public PerfilUsuario {
 public:
-    
-     /// @brief Construtor da classe Administrador
-     /// @param nome Nome do Administrador
-     /// @param email E-mail do Administrador
+    /**
+     * @brief Construtor da classe Administrador.
+     *
+     * @param nome O nome do administrador.
+     * @param email O email do administrador.
+     */
     Administrador(const std::string& nome, const std::string& email);
 
-     /// @brief Salva as informações do Administrador em um arquivo CSV
-     /// @return 0 em caso de sucesso, -1 em caso de falha
+    /**
+     * @brief Salva as informações do administrador no arquivo CSV.
+     *
+     * @return 0 em caso de sucesso, -1 em caso de erro.
+     */
     int salvarAdm();
 
-     /// @brief Verifica se um Administrador já existe no arquivo CSV
-     /// @return True se o Administrador já existe, False caso contrário
+    /**
+     * @brief Verifica se um administrador com as mesmas informações já existe.
+     *
+     * @return true se o administrador já existe, false caso contrário.
+     */
     bool admJaExiste() const;
 
-    
-     /// @brief Realiza o cadastro de um novo Administrador
-    static void cadastroAdm();
+    /**
+     * @brief Realiza o cadastro de um novo administrador.
+     *
+     * Solicita informações do usuário, verifica se o administrador já existe
+     * e, se não existir, salva as informações no arquivo CSV.
+     */
+    void cadastroAdm();
 };
 
 #endif // ADMINISTRADOR_HPP
