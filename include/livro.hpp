0034486fe20c05847ceb08ff1abaf3729d08c577
gustavovdeandrade <148797062+gustavovdeandrade@users.noreply.github.com>
2023-12-02 11:09:21 -0300

    Update livro.hpp

--- include/livro.hpp
+++ include/livro.hpp
@@ -1,28 +1,78 @@
 #ifndef LIVRO_HPP
 #define LIVRO_HPP
+
 #include <iostream>
 #include <string>
 
+/**
+ * @brief Representa um objeto Livro.
+ *
+ * Esta classe encapsula informações sobre um livro, incluindo título, autor,
+ * ano de publicação e disponibilidade de aluguel.
+ */
 class Livro {
 public:
+    /**
+     * @brief Construtor da classe Livro.
+     *
+     * @param titulo           O título do livro.
+     * @param autor            O autor do livro.
+     * @param anoPublicacao    O ano de publicação do livro.
+     */
     Livro(const std::string& titulo, const std::string& autor, int anoPublicacao);
 
-    // @brief informações necessárias que o livro terá
+    /**
+     * @brief Obtém o título do livro.
+     *
+     * @return O título do livro.
+     */
     std::string getTitulo() const;
+
+    /**
+     * @brief Obtém o autor do livro.
+     *
+     * @return O autor do livro.
+     */
     std::string getAutor() const;
+
+    /**
+     * @brief Obtém o ano de publicação do livro.
+     *
+     * @return O ano de publicação do livro.
+     */
     int getAnoPublicacao() const;
+
+    /**
+     * @brief Verifica se o livro está disponível para aluguel.
+     *
+     * @return true se o livro estiver disponível, false caso contrário.
+     */
     bool estaAlugado() const;
 
-    // @brief funções de alteração dos dados existentes de um livro
+    /**
+     * @brief Altera as informações do livro.
+     *
+     * @param novoTitulo        O novo título do livro.
+     * @param novoAutor         O novo autor do livro.
+     * @param novoAnoPublicacao O novo ano de publicação do livro.
+     */
     void alterarLivro(const std::string& novoTitulo, const std::string& novoAutor, int novoAnoPublicacao);
+
+    /**
+     * @brief Marca o livro como alugado.
+     */
     void alugar();
+
+    /**
+     * @brief Marca o livro como disponível para aluguel.
+     */
     void devolver();
 
 private:
-    std::string titulo;
-    std::string autor;
-    int anoPublicacao;
-    bool disponivel;
+    std::string titulo;          ///< O título do livro.
+    std::string autor;           ///< O autor do livro.
+    int anoPublicacao;           ///< O ano de publicação do livro.
+    bool disponivel;             ///< Indica se o livro está disponível para aluguel.
 };
 
 #endif // LIVRO_HPP
