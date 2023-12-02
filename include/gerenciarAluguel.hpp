a13d88853afb8afb2fcf948209043ce7235a8e40
gustavovdeandrade <148797062+gustavovdeandrade@users.noreply.github.com>
2023-12-02 11:10:40 -0300

    Update gerenciarAluguel.hpp

--- include/gerenciarAluguel.hpp
+++ include/gerenciarAluguel.hpp
@@ -1,4 +1,3 @@
-
 #ifndef GERENCIAR_ALUGUEL_HPP
 #define GERENCIAR_ALUGUEL_HPP
 
@@ -7,15 +6,52 @@
 #include <string>
 #include "livro.hpp"
 
+/**
+ * @brief Classe para gerenciar o aluguel de livros.
+ *
+ * Esta classe fornece funcionalidades para alugar, devolver, aumentar prazo,
+ * definir multa e verificar a disponibilidade de livros.
+ */
 class GerenciarAluguel {
 public:
-    static std::vector<Livro> livros; 
-    //funções para alugar, devolver o livro, aumentar prazo e definir multa, conferir disponibilidade
+    static std::vector<Livro> livros; ///< Lista de livros disponíveis para aluguel.
 
+    /**
+     * @brief Aluga um livro específico.
+     *
+     * @param titulo O título do livro a ser alugado.
+     */
     static void alugarLivro(const std::string& titulo);
+
+    /**
+     * @brief Devolve um livro previamente alugado.
+     *
+     * @param titulo O título do livro a ser devolvido.
+     */
     static void devolverLivro(const std::string& titulo);
+
+    /**
+     * @brief Aumenta o prazo de devolução de um livro.
+     *
+     * @param titulo O título do livro para o qual o prazo será aumentado.
+     * @param dias   O número de dias a serem adicionados ao prazo de devolução.
+     */
     static void aumentarPrazo(const std::string& titulo, int dias);
+
+    /**
+     * @brief Define a multa para um livro específico.
+     *
+     * @param titulo O título do livro para o qual a multa será definida.
+     * @param multa  O valor da multa a ser aplicada.
+     */
     static void definirMulta(const std::string& titulo, double multa);
+
+    /**
+     * @brief Verifica a disponibilidade de um livro.
+     *
+     * @param titulo O título do livro a ser verificado.
+     * @return true se o livro estiver disponível, false caso contrário.
+     */
     static bool checarDisponibilidade(const std::string& titulo);
 };
 
