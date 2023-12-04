837f79c4aa01c9d20df81679d8e23e79c1e2f21b
marinavaladao <148727212+marinavaladao@users.noreply.github.com>
2023-12-04 20:58:18 -0300

    Update aluguel.cpp

--- cpps/aluguel.cpp
+++ cpps/aluguel.cpp
@@ -1,19 +1,51 @@
 #include "aluguel.hpp"
 #include "livro.hpp"
+#include "acervo.hpp"
 
-void Aluguel::checarDisponibilidade(const std::string& titulo) {
-    for (auto& livro : livros) {
-        if (livro.getTitulo() == titulo) {
-            if (livro.estaAlugado()) {
-                std::cout << "O livro está alugado. Aluguel não disponível." << std::endl;
-            } else {
-                std::cout << "O livro está disponível para aluguel." << std::endl;
-            }
-            return;
-        }
-    }
-    std::cout << "Livro não encontrado." << std::endl;
+Aluguel::Aluguel(std::string titulo, std::string email)
+    : prazo(1), multa(0.0),titulo(titulo), email(email) {}
+
+void Aluguel::definirPrazo(int dias) {
+    prazo = dias;
 }
-    // Se o livro não foi encontrado
-    std::cout << "Livro não encontrado. Aluguel não efetuado." << std::endl;
+
+void Aluguel::aumentarPrazo(int dias) {
+    prazo += dias;
 }
+
+void Aluguel::definirMulta(double novaMulta) {
+    multa = novaMulta;
+}
+
+int Aluguel::getPrazoDevolucao() {
+    return prazo;
+}
+
+std::string Aluguel::getTitulo() const{
+     return titulo;
+}
+
+void Aluguel::deCSV(const std::string& linha){
+  std::istringstream ss(linha);
+  std::string aux;
+
+  std::cout << "deCSV "<< linha << std::endl;
+  std::getline(ss,titulo,',');
+  std::getline(ss,email,',');
+  std::getline(ss,aux,',');
+  prazo = std::stoi(aux);
+  std::getline(ss,aux,',');
+  multa = std::stod(aux);
+}
+
+std::string Aluguel::paraCSV() const{
+  std::ostringstream oss;
+
+  oss << titulo << ", "
+      << email << ", "
+      << prazo << ", "
+      << multa;
+
+  return oss.str();
+}
+
