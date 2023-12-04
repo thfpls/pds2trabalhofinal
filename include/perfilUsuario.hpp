93d99f6865a1ac7ed245f2906bfae40a62d14554
manuanp <148726428+manuanp@users.noreply.github.com>
2023-12-04 16:52:31 -0300

    Update perfilUsuario.hpp

--- include/perfilUsuario.hpp
+++ include/perfilUsuario.hpp
@@ -4,9 +4,6 @@
 
 #include <iostream>
 
-class Aluno; // Declarando a classe Aluno para evitar referência circular**
-
-
 /// @brief Enumeração que representa o papel do usuário
 enum Papel_do_usuario
 {
@@ -20,6 +17,7 @@ class PerfilUsuario
 private:
     std::string _nome_perfil_usuario;  
     std::string _email_perfil_usuario; 
+    int _livrosAlugados;
 protected:
     Papel_do_usuario _papel; 
 public:
@@ -27,7 +25,7 @@ public:
      /// @brief Construtor da classe PerfilUsuario
      /// @param nome Nome do usuário
      /// @param email E-mail do usuário
-    PerfilUsuario(const std::string& nome, const std::string& email);
+    PerfilUsuario(const std::string& nome, const std::string& email, const int livros);
 
     /// @brief Destrutor virtual da classe PerfilUsuario
     virtual ~PerfilUsuario();
@@ -40,13 +38,42 @@ public:
      // @return E-mail do usuário
     const std::string& getEmailUsuario() const;
 
+     /// @brief Obtém o número de livros alugados
+     // @return livros alugados 
+    int getLivrosAlugados() const;
+
      /// @brief Obtém o papel do usuário
      /// @return Papel do usuário (ADMIN ou ALUNO)
     Papel_do_usuario getPapelUsuario() const;
 
+     /// @brief Define o nome do usuário
+     /// @return nenhum
+    void setNomeUsuario(std::string nome);
+
+     /// @brief Defin o e-mail do usuário
+     // @return nenhum
+    void setEmailUsuario(std::string email);
+
+     /// @brief Define o número de livros alugados
+     // @return nenhum
+    void setLivrosAlugados(int livrosAlugados);
+
+     /// @brief Define o papel do usuário (ADMIN ou ALUNO)
+     /// @return nenhum 
+    void setPapelUsuario(Papel_do_usuario papel);
+
     /// @brief Converte o papel do usuário para uma string
     ///@return String representando o papel do usuário
     std::string papelToString() const;
+
+    void deCSV(const std::string& linha);
+
+    void deCIN();
+
+    std::string paraCSV();
+    // para tornar a classe polimorfica
+
+    virtual void apenasparatornarpolimorfica() const;
 };
 
 #endif // PERFILUSUARIO_HPP
