413fb635b8ced721102eb2121cbf1e2d0b6b9775
marinavaladao <148727212+marinavaladao@users.noreply.github.com>
2023-12-04 09:37:46 -0300

    Update gerenciarUsuário.hpp

--- include/gerenciarUsuário.hpp
+++ include/gerenciarUsuário.hpp
@@ -1,8 +1,18 @@
-//#include "usuario.hpp"
+#include "const.hpp"
+#include "perfilUsuario.hpp"
 #include <string>
+#include <vector>
 
-class gerenciarUsuario {
+class GerenciarUsuario {
   public:
+
+  void carregarUsuarios(std::string nomeArquivo);
+
+  void salvarUsuarios(std::string nomeArquivo);
+
+  void inserirUsuario();
+
+  PerfilUsuario localizarUsuario(std::string email);
   
   // @brief deleta o usuario pelo id
   // @param email do usuario
@@ -16,15 +26,17 @@ class gerenciarUsuario {
   // @brief alterar os dados de um perfil
   // @param email, nome, livros alugados do usuário
 
-  void alterarPerfil (const std::string& email, const std::string& novoNome, const std::string& novosLivrosAlugados);
+  void alterarPerfil (const std::string& email, const std::string& novoNome, const int novosLivrosAlugados);
 
   //verificar se existe algum perfil com o email de entrada
   //se existir, igualas todos os seus dados para os que entram na função acima
 
   // @brief imprimir todos os perfis
 
-  char imprimirPerfis ();
+  void imprimirPerfis ();
 
-  // imprimir todos os cadastros que estão guardados no arquivo
+private:
+
+std::vector<PerfilUsuario> usuarios;
 
 };
