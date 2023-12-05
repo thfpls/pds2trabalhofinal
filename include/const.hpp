8be0b5ea9bf250e0766bb6adda8b4bc8a92a00f6
manuanp <148726428+manuanp@users.noreply.github.com>
2023-12-04 21:40:15 -0300

    Update const.hpp

---
+++ include/const.hpp
@@ -0,0 +1,6 @@
+#define ARQLIVRO "files/livros.csv"
+#define ARQLIVROTEMP "files/livros_temp.csv"
+#define ARQUSUARIO "files/usuarios.csv"
+#define ARQUSUARIOTEMP "files/usuarios_temp.csv"
+#define ARQALUGUEL "files/alugueis.csv"
+#define ARQALUGUELTEMP "files/alugueis_temp.csv"
