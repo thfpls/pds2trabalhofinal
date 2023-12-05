3730a9f867595876b83807cd90ae0c4aa02f0532
manuanp <148726428+manuanp@users.noreply.github.com>
2023-12-04 21:37:16 -0300

    Update acervo.hpp

--- include/acervo.hpp
+++ include/acervo.hpp
@@ -1,11 +1,12 @@
 #ifndef ACERVO_BIBLIOTECA_HPP
 #define ACERVO_BIBLIOTECA_HPP
 
+#include "const.hpp"
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include "Livro.hpp"
+#include "livro.hpp"
 
 /**
  * @brief Classe para gerenciar o acervo de uma biblioteca.
@@ -15,6 +16,8 @@
  */
 class AcervoBiblioteca {
 public:
+    void carregarAcervo(std::string nomeArquivo);
+    void salvarAcervo(std::string nomeArquivo);
     /**
      * @brief Insere um novo livro no acervo.
      *
@@ -37,6 +40,11 @@ public:
      */
     void buscarLivro() const;
 
+    Livro localizarLivro(const std::string titulo) const;
+
+    void atualizarLivro(const Livro liv) ;
+
+
 private:
     /**
      * @brief Armazena os livros presentes no acervo.
@@ -50,7 +58,7 @@ private:
      * @param autor           O autor do livro.
      * @param anoPublicacao   O ano de publicação do livro.
      */
-    void botarNoVetor(std::string titulo, std::string autor, int anoPublicacao);
+    void inserirNoVetor(std::string titulo, std::string autor, int anoPublicacao);
 };
 
 #endif // ACERVO_BIBLIOTECA_HPP
